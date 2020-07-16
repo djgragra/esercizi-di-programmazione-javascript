32b4a8b949f7d847530fe45e96feeb2536f6b47c
Graziano Melzi <65872226+djgragra@users.noreply.github.com>
2020-07-16 22:08:46 +0200

    Update gatti-in-fila.js

--- esercizi-stringhe-numeri/gatti-in-fila.js
+++ esercizi-stringhe-numeri/gatti-in-fila.js
@@ -18,4 +18,11 @@
   http://www.imparareaprogrammare.it
 */
 
-var cats = 44
+var gatti = 44;
+var gattixfila = 6;
+var file = Math.ceil(gatti/gattixfila);
+var gattitot = gattixfila*file;
+var gattimancanti = gattitot-gatti
+//console.log('Ci sono ' + gatti + ' gatti disposti su ' + file + ' file e nell ultima mancano ' + gattimancanti + ' gatti');
+console.log('Ci sono ${gatti} gatti disposti su ${file} file e nell ultima mancano ${gattimancanti} gatti');
+
