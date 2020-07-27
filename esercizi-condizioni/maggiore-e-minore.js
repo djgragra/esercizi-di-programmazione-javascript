e6a5780c0d00d2e87baba46c629f51886a6ef9d1
djgragra <65872226+djgragra@users.noreply.github.com>
2020-07-27 15:14:21 +0200

    Update maggiore-e-minore.js

--- esercizi-condizioni/maggiore-e-minore.js
+++ esercizi-condizioni/maggiore-e-minore.js
@@ -6,7 +6,57 @@
   Esempio:
     Input: a = 3, b = -1, c = 4, d = -2
     Output: maggiore = 4, minore = -2
-    
+
 
   http://www.imparareaprogrammare.it
 */
+
+
+var a = 3;
+var b = -1;
+var c = 4;
+var d = -2;
+
+// trovo il numero maggiore
+
+if (a >= b && a >= c && a >= d){
+console.log('Il numero maggiore è ' + a)
+
+}
+
+if (b >= a && b >= c && b >= d){
+console.log('Il numero maggiore è ' + b)
+
+}
+
+if (c >= a && c >= b && c >= d){
+console.log('Il numero maggiore è ' + c)
+
+}
+
+if (d >= a && d >= b && d >= c){
+console.log('Il numero maggiore è ' + d)
+
+}
+
+// trovo il numero minore
+
+if (a <= b && a <= c && a <= d){
+console.log('Il numero minore è ' + a)
+
+}
+
+if (b <= a && b <= c && b <= d){
+console.log('Il numero minore è ' + b)
+
+}
+
+if (c <= a && c <= b && c <= d){
+console.log('Il numero minore è ' + c)
+
+}
+
+if (d <= a && d <= b && d <= c){
+console.log('Il numero minore è ' + d)
+
+}
