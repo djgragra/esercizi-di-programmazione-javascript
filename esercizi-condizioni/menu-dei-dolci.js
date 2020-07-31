4b17f7cb6afa92bdff2c56a9aeac36954a7cdd72
Graziano Melzi <grazianomelzi@gmail.com>
2020-07-31 07:55:33 +0200

    Update menu-dei-dolci.js

--- esercizi-condizioni/menu-dei-dolci.js
+++ esercizi-condizioni/menu-dei-dolci.js
@@ -21,3 +21,39 @@
 
   http://www.imparareaprogrammare.it
 */
+
+
+var dolce1 = '1. Tiramisù';
+var dolce2 = '2. Torta della nonna';
+var dolce3 = '3. Cheesecake alla nutella';
+var dolce4 = '4. Macedonia';
+console.log(dolce1);
+console.log(dolce2);
+console.log(dolce3);
+console.log(dolce4);
+
+dolce1 = 'Tiramisù';
+dolce2 = 'Torta della nonna';
+dolce3 = 'Cheesecake alla nutella';
+dolce4 = 'Macedonia';
+
+var x = 1;
+
+
+switch (x) {
+  case 1:
+    console.log('Hai scelto ' + dolce1);
+    break;
+  case 2:
+    console.log('Hai scelto ' + dolce2);
+    break;
+  case 3:
+    console.log('Hai scelto ' + dolce3);
+    break;
+  case 4:
+    console.log('Hai scelto ' + dolce4);
+    break;
+  default:
+    console.log('Dolce non disponibile');
+
+}
